65a208b023cdc7feb2d52fa21718fce2f4e142c2
Ralf Beckers <ralf.beckers@elektrobit.com>
2020-04-08 11:45:26 +0200

    add Core 3.1 ver 2.2

--- JenkinsWebApiUwp/Properties/AssemblyInfo.cs
+++ JenkinsWebApiUwp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JenkinsWebApiUwp")]
+[assembly: AssemblyTitle("JenkinsWebApi")]
 [assembly: AssemblyDescription("API to communicate with Jenkins")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ralf Beckers")]
 [assembly: AssemblyProduct("JenkinsWebApi")]
-[assembly: AssemblyCopyright("Copyright © Ralf Beckers 2017 - 2019")]
+[assembly: AssemblyCopyright("Copyright © Ralf Beckers 2017 - 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
 [assembly: ComVisible(false)]
