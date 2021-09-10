dd3c1c198be89f11babe3a2c92200b174669bacd
Bassman2 <ralf.beckers@hotmail.com>
2021-09-10 22:39:35 +0200

    change to curl

--- Generator/Schema/work.cmd
+++ Generator/Schema/work.cmd
@@ -1,57 +1,61 @@
-dl -u dl -p dl -o hudson.xsd http://localhost:8080/api/schema
-dl -u dl -p dl -o defaultCrumbIssuer.xsd http://localhost:8080/crumbIssuer/api/schema
-dl -u dl -p dl -o localPluginManager.xsd http://localhost:8080/pluginManager/api/schema
-dl -u dl -p dl -o rootActionImpl.xsd http://localhost:8080/credentials/api/schema
+set JENKINSURL=http://tiny:8080
+set JENKINSUSER=Tester:tester 
+
+curl --user %JENKINSUSER% -o hudson.xsd %JENKINSURL%/api/schema
+
+curl --user %JENKINSUSER% -o defaultCrumbIssuer.xsd %JENKINSURL%/crumbIssuer/api/schema
+curl --user %JENKINSUSER% -o localPluginManager.xsd %JENKINSURL%/pluginManager/api/schema
+curl --user %JENKINSUSER% -o rootActionImpl.xsd %JENKINSURL%/credentials/api/schema
 
 
 rem user
-dl -u dl -p dl -o asyncPeople.xsd http://localhost:8080/asynchPeople/api/schema
-dl -u dl -p dl -o people.xsd http://localhost:8080/people/api/schema
-dl -u dl -p dl -o user.xsd http://localhost:8080/user/bs/api/schema
+curl --user %JENKINSUSER% -o asyncPeople.xsd %JENKINSURL%/asynchPeople/api/schema
+curl --user %JENKINSUSER% -o people.xsd %JENKINSURL%/people/api/schema
+curl --user %JENKINSUSER% -o user.xsd %JENKINSURL%/user/bs/api/schema
 
 
 rem views
-dl -u dl -p dl -o categorizedJobsView.xsd  http://localhost:8080/view/Categorization/api/schema
-dl -u dl -p dl -o listView.xsd http://localhost:8080/view/Test/api/schema
-dl -u dl -p dl -o myView.xsd "http://localhost:8080/view/my view/api/schema"
-dl -u dl -p dl -o dashboard.xsd "http://localhost:8080/view/Dashboard/api/schema"
-dl -u dl -p dl -o allView.xsd "http://localhost:8080/view/all/api/schema"
+curl --user %JENKINSUSER% -o categorizedJobsView.xsd  %JENKINSURL%/view/Categorization/api/schema
+curl --user %JENKINSUSER% -o listView.xsd %JENKINSURL%/view/Test/api/schema
+curl --user %JENKINSUSER% -o myView.xsd "%JENKINSURL%/view/my view/api/schema"
+curl --user %JENKINSUSER% -o dashboard.xsd "%JENKINSURL%/view/Dashboard/api/schema"
+curl --user %JENKINSUSER% -o allView.xsd "%JENKINSURL%/view/all/api/schema"
 
 rem jobs & builds
-dl -u dl -p dl -o externalJob.xsd "http://localhost:8080/job/External Job/api/schema"
+curl --user %JENKINSUSER% -o externalJob.xsd "%JENKINSURL%/job/External Job/api/schema"
 
-dl -u dl -p dl -o folder.xsd "http://localhost:8080/job/Folder/api/schema"
+curl --user %JENKINSUSER% -o folder.xsd "%JENKINSURL%/job/Folder/api/schema"
 
-dl -u dl -p dl -o freeStyleProject.xsd "http://localhost:8080/job/Freestyle Test Doc/api/schema"
-dl -u dl -p dl -o freeStyleBuild.xsd "http://localhost:8080/job/Freestyle Test Doc/2/api/schema"
+curl --user %JENKINSUSER% -o freeStyleProject.xsd "%JENKINSURL%/job/Freestyle Test Doc/api/schema"
+curl --user %JENKINSUSER% -o freeStyleBuild.xsd "%JENKINSURL%/job/Freestyle Test Doc/2/api/schema"
 
-dl -u dl -p dl -o organizationFolder.xsd http://localhost:8080/job/GitHub/api/schema
+curl --user %JENKINSUSER% -o organizationFolder.xsd %JENKINSURL%/job/GitHub/api/schema
 
-dl -u dl -p dl -o workflowMultiBranchProject.xsd "http://localhost:8080/job/Multibranch Pipeline/api/schema"
+curl --user %JENKINSUSER% -o workflowMultiBranchProject.xsd "%JENKINSURL%/job/Multibranch Pipeline/api/schema"
 
-dl -u dl -p dl -o matrixProject.xsd http://localhost:8080/job/Multiconfiguration/api/schema
-dl -u dl -p dl -o matrixBuild.xsd http://localhost:8080/job/Multiconfiguration/lastSuccessfulBuild/api/schema
+curl --user %JENKINSUSER% -o matrixProject.xsd %JENKINSURL%/job/Multiconfiguration/api/schema
+curl --user %JENKINSUSER% -o matrixBuild.xsd %JENKINSURL%/job/Multiconfiguration/lastSuccessfulBuild/api/schema
 
-dl -u dl -p dl -o workflowJob.xsd http://localhost:8080/job/Pipeline/api/schema
-dl -u dl -p dl -o workflowRun.xsd http://localhost:8080/job/Pipeline/1/api/schema
+curl --user %JENKINSUSER% -o workflowJob.xsd %JENKINSURL%/job/Pipeline/api/schema
+curl --user %JENKINSUSER% -o workflowRun.xsd %JENKINSURL%/job/Pipeline/1/api/schema
 
-dl -u dl -p dl -o multijobJob.xsd http://localhost:8080/job/MultiJob/api/schema
-dl -u dl -p dl -o multijobRun.xsd http://localhost:8080/job/MultiJob/1/api/schema
+curl --user %JENKINSUSER% -o multijobJob.xsd %JENKINSURL%/job/MultiJob/api/schema
+curl --user %JENKINSUSER% -o multijobRun.xsd %JENKINSURL%/job/MultiJob/1/api/schema
 
 rem test result
-dl -u dl -p dl -o testResult.xsd "http://localhost:8080/job/Freestyle Test Doc/9/testReport/api/schema"
-dl -u dl -p dl -o caseResult.xsd "http://localhost:8080/job/Freestyle Test Doc/lastCompletedBuild/testReport/UT_Av2UnitTest/xml/_empty_/api/schema"
-dl -u dl -p dl -o packageResult.xsd "http://localhost:8080/job/Freestyle Test Doc/lastCompletedBuild/testReport/UT_Av2UnitTest/api/schema"
+curl --user %JENKINSUSER% -o testResult.xsd "%JENKINSURL%/job/Freestyle Test Doc/9/testReport/api/schema"
+curl --user %JENKINSUSER% -o caseResult.xsd "%JENKINSURL%/job/Freestyle Test Doc/lastCompletedBuild/testReport/UT_Av2UnitTest/xml/_empty_/api/schema"
+curl --user %JENKINSUSER% -o packageResult.xsd "%JENKINSURL%/job/Freestyle Test Doc/lastCompletedBuild/testReport/UT_Av2UnitTest/api/schema"
 
 rem new new new 
-dl -u dl -p dl -o computerSet.xsd http://localhost:8080/computer/api/schema
-dl -u dl -p dl -o masterComputer.xsd http://localhost:8080/computer/(master)/api/schema
-dl -u dl -p dl -o slaveComputer.xsd http://localhost:8080/computer/b00014/api/schema
-dl -u dl -p dl -o label.xsd http://localhost:8080/label/XXX_LABEL/api/schema
-dl -u dl -p dl -o userFacingAction.xsd http://localhost:8080/credentials/store/system/api/schema
-dl -u dl -p dl -o queue.xsd http://localhost:8080/queue/api/schema
+curl --user %JENKINSUSER% -o computerSet.xsd %JENKINSURL%/computer/api/schema
+curl --user %JENKINSUSER% -o masterComputer.xsd %JENKINSURL%/computer/(master)/api/schema
+curl --user %JENKINSUSER% -o slaveComputer.xsd %JENKINSURL%/computer/b00014/api/schema
+curl --user %JENKINSUSER% -o label.xsd %JENKINSURL%/label/XXX_LABEL/api/schema
+curl --user %JENKINSUSER% -o userFacingAction.xsd %JENKINSURL%/credentials/store/system/api/schema
+curl --user %JENKINSUSER% -o queue.xsd %JENKINSURL%/queue/api/schema
 
 rem plugins
-dl -u dl -p dl -o injectedEnvVars.xsd http://localhost:8080/job/Downstream_A_Test/2/injectedEnvVars/api/schema
-dl -u dl -p dl -o buildGraph.xsd http://localhost:8080/job/Downstream_A_Test/2/BuildGraph/api/schema
+curl --user %JENKINSUSER% -o injectedEnvVars.xsd %JENKINSURL%/job/Downstream_A_Test/2/injectedEnvVars/api/schema
+curl --user %JENKINSUSER% -o buildGraph.xsd %JENKINSURL%/job/Downstream_A_Test/2/BuildGraph/api/schema
 pause
